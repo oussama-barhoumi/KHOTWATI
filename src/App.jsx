8cb7e4002e30a8f12245458fd0b1c0c03cba56a9
NourelhoudaOKMID <nour.okmid@gmail.com>
2026-02-20 14:35:57 +0000

    import the useeffect and the useappstore

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import {AnimatePresence} from 'framer-motion'
-
 import { Home } from './pages/Home'
+import { useAppStore } from './store/UseAppStore'
+import { useEffect } from 'react'
 
 
 export const App = () => {
