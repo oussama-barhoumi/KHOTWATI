f7ee431c92787a68925feb9596444800178340ad
ayoub <ab.naciri.official@gmail.com>
2026-02-21 19:14:49 +0000

    fix : fixed routing conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -29,18 +29,11 @@ export const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-<<<<<<< HEAD
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/groups" element={<Groups />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/chat" element={<Chat />} />
-        <Route path="/tmp" element={<Tempchat />} />
-=======
         <Route path="/feed" element={<ProtectedRoute><Feed /></ProtectedRoute>} />
         <Route path="/groups" element={<ProtectedRoute><Groups /></ProtectedRoute>} />
         <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
         <Route path="/chat" element={<ProtectedRoute><Chat /></ProtectedRoute>} />
->>>>>>> 53e501dcd4c07931986d410e23995f8509ddcc98
+        <Route path="/tmp" element={<Tempchat/>} />
       </Routes>
     </AnimatePresence>
     </>
